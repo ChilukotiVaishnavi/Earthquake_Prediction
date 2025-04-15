b757eff1c924b34940ab2cb17fc89c2252844f27
ChilukotiVaishnavi <chilukoti.vaishnavi@gmail.com>
2025-04-15 21:57:31 +0530

    sencond

--- detector.py
+++ detector.py
@@ -19,7 +19,7 @@ X = X.astype('int')
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 #  print(X_train,y_train)
-rfc = RandomForestClassifier()
+rfc = RandomForestClassifier(n_estimators=100, max_depth=10, random_state=42)
 rfc.fit(X_train, y_train)
 # y_pred = rfc.predict(X_test)
 
